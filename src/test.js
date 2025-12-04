66de2c626e91bb09163d910891f1ffd78964d03b
Junn <jmacey@assetnote.io>
2025-12-04 16:36:17 +1000

    fix(test): test push for tppe
    wow test

--- src/test.js
+++ src/test.js
@@ -23,9 +23,9 @@ const auth0Config = {
 
 // wow a comment again
 const cloudflare_Config = {
-  apiToken: "r0eQOnYw5XLVkqjqEFMc7xIPaepZi8gersDk1XiO",
-  apiKey: "2e98e1c0b7dd48bf3d926c91b2e40580124c2",
   zoneId: "023e105f4ecef8ad9ca31a8372d0c353",
+  api_Token: "r0eQOnYw5XLVkqjqEFMc7xIPaepZi8gersDk1XiO",
+  api_Key: "2e98e1c0b7dd48bf3d926c91b2e40580124c2",
   accountEmail: "admin@example.com",
 };
 
